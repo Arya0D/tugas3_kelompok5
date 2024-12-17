a7c1a86b50d17d9ca44ea1d9818acfc9f7167f92
Arya Dirham Wijaya Kusumah <120310980+Arya0D@users.noreply.github.com>
2024-12-17 16:33:35 +0700

    Reservation (#13)
    * add reservation
    
    * update reservation & add dashboard

---
+++ app/views/dashboard.php
@@ -0,0 +1,25 @@
+<div class="flex flex-col justify-center items-center h-full bg-[url('/images/background.png')] bg-cover">
+    <div class="bg-slate-400/80 text-center w-1/2 text-xl font-bold p-6 rounded-xl">
+        <h1 class="text-3xl">Selamat Datang</h1>
+        <h3>Admin</h3>
+    </div>
+    <div class="grid max-md:grid-cols-1 max-md:grid-rows-1 grid-cols-2 grid-rows-2">
+        <a href="/user/index" class="w-8 h-8 p-4 w-full h-full">
+            <img src="/images/user.png" alt="" class="w-[150px] h-[150px]">
+            <h1 class="text-center font-bold text-white text-xl">User</h1>
+        </a>
+        <a href="/reservation/index" class="w-8 h-8 p-4 w-full h-full">
+            <img src="/images/reservation.png" alt="" class="w-[150px] h-[150px]">
+            <h1 class="text-center font-bold text-white text-xl">Reservation</h1>
+        </a>
+        <a href="/activites/index" class="w-8 h-8 p-4 w-full h-full">
+            <img src="/images/activities.png" alt="" class="w-[150px] h-[150px]">
+            <h1 class="text-center font-bold text-white text-xl">Activities</h1>
+        </a>
+        <a href="/accommodation/" class="w-8 h-8 p-4 w-full h-full">
+            <img src="/images/accommodations.png" alt="" class="w-[150px] h-[150px]">
+            <h1 class="text-center font-bold text-white text-xl">Accommodations</h1>
+        </a>
+        
+    </div>
+</div>
